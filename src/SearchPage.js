bcb22033cb5e7ab170038637ccb9db0995ce4ffc
SuA Kang <cocosua88@gmail.com>
2020-04-06 00:39:40 -0400

    Add SearchPage.JS

---
+++ src/SearchPage.js
@@ -0,0 +1,8 @@
+nimport React, { Component } from "react";
+import { Link } from "react-router-dom";
+//import * as BooksAPI from "./BooksAPI";
+import "./index.css";
+
+class SearchPage extends Component {}
+
+export default SearchPage;
